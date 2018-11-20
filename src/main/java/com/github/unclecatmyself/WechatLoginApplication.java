955422807c75df3751343afb5a63fa3b4f75a625
胡宏韬 <3183764662@qq.com>
2018-11-20 16:01:54 +0800

    update

--- src/main/java/com/github/unclecatmyself/WechatLoginApplication.java
+++ src/main/java/com/github/unclecatmyself/WechatLoginApplication.java
@@ -2,13 +2,9 @@ package com.github.unclecatmyself;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScans;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2 //Swagger自动生成文档
 @ComponentScan({"com.myself.winter"})
 @ComponentScan({"com.github.unclecatmyself"})
 public class WechatLoginApplication {
